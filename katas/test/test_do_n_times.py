14ab7c4c808945e48a9b3af3951e90141caa4d50
Aseel <aseelarr@gmail.com>
2025-04-22 19:10:41 +0000

    Do_n_times solution

---
+++ katas/test/test_do_n_times.py
@@ -0,0 +1,14 @@
+import unittest
+import random
+from PythonKatasFursa.katas.do_n_times import   do_n_times
+
+
+class TestDoNTimes(unittest.TestCase):
+    def setUp(self):
+        self.count=0
+    def counter(self):
+        self.count+=1
+    def test_n_times(self):
+        ran_num=random.randint(0,100)
+        do_n_times(self.counter,ran_num)
+        self.assertEqual(self.count,ran_num)
