7c347433f7b04876a085ff1cda8150af4c0c476e
Aseel <aseelarr@gmail.com>
2025-04-24 17:58:58 +0000

    Matrix rotate solution

--- katas/test/test_matrix_rotate.py
+++ katas/test/test_matrix_rotate.py
@@ -11,3 +11,7 @@ class TestRotateMatrix(unittest.TestCase):
         rotate_matrix(matrix2)
         self.assertEqual(matrix2,[[9,7,1],[5,5,2],[1,3,3]])
         print(matrix2)
+    def test_2(self):
+        matrix3=  [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+        rotate_matrix(matrix3)
+        self.assertEqual(matrix3,[[7,4,1],[8,5,2],[9,6,3]])
